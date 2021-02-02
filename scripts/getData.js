97308ad069a97df449f2bbce772d75fbef568747
Sjors Wijsman <sjorswijsman@gmail.com>
2021-02-02 15:19:35 +0100

    Added basic html

---
+++ scripts/getData.js
@@ -0,0 +1,5 @@
+const clientId = "71c1e592b56c424caf9714524949cf26";
+const userId = "miwirosj";
+const baseUrl = "https://api.spotify.com/v1";
+const userUrl = `${baseUrl}/users/${userId}`;
+const playlistUrl = `${userUrl}/playlists`;
