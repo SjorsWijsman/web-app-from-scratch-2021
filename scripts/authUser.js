92cfb94ccb7ed3a66ba22c7715ae79ade22725b6
Sjors Wijsman <sjorswijsman@gmail.com>
2021-02-08 15:36:59 +0100

    Get User Data on authentication

--- scripts/authUser.js
+++ scripts/authUser.js
@@ -1,15 +1,16 @@
 // code inspired by: https://github.com/mujibsardar/spotify_jquery_only/blob/master/script.js
 // https://www.youtube.com/watch?v=d0FFlTeyAY8
+import { getData } from "./getData.js";
 
 // Authorizes the user and redirects if necessary
 export async function authUser(forceRedirect = false) {
   const clientId = "71c1e592b56c424caf9714524949cf26";
-  const redirectUrl = "https%3A%2F%2Fsjorswijsman.github.io%2Fweb-app-from-scratch-2021";
+  // const redirectUrl = "https%3A%2F%2Fsjorswijsman.github.io%2Fweb-app-from-scratch-2021";
   const authRedirect = `https://accounts.spotify.com/authorize` +
     `?client_id=${clientId}` +
     `&scope=playlist-read-private` +
     `&response_type=token` +
-    `&redirect_uri=${redirectUrl}`;
+    `&redirect_uri=${encodeURIComponent(window.location)}`;
 
   // Force redirect to spotify authorization
   if (forceRedirect) {
@@ -21,6 +22,7 @@ export async function authUser(forceRedirect = false) {
   getAccessToken();
   const accessToken = localStorage.getItem("ranker-hash");
   if (accessToken) {
+    getUserData(accessToken)
     return accessToken;
   } else {
     window.location.replace(authRedirect);
@@ -39,3 +41,9 @@ function getAccessToken() {
     window.location = window.location.href.split('#')[0];
   }
 };
+
+function getUserData() {
+  getData(`https://api.spotify.com/v1/me`, (data) => {
+    console.log(data)
+  })
+}
